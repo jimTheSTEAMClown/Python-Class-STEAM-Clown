d900ec0cc34a8b730f21fd09fda339f9754cf8ad
Jim Burnham <jim.the.steam.clown@gmail.com>
2025-03-31 19:42:22 -0700

    Create Ch9-Read-Write-Files-Lab#4--file-rstrip.py

---
+++ Ch9-Read-Write-Files-Lab#4--file-rstrip.py
@@ -0,0 +1,28 @@
+# Opening File Handles and reading data from files
+print('''
+This Lab is about opening a file handle, and reading 
+the file, printing line by line, and striping the 
+extra /n at the end of a line in a file
+      ''')
+# Challenges
+print('''
+Challenge #1
+----------------------------------------------------
+#1 - Edit the code below to not print 
+     the extra "\\n" on each line
+Hint: Use a String Method that will "strip" a character. 
+- https://www.w3schools.com/python/python_strings_methods.asp
+
+#2 - Edit the code below to open the mbox-short.txt file
+---
+''')
+# -------------------------------------------------
+print('''Answer to Challenges
+-------------------------------------------------''')
+# -----------------------------
+fhand = open('text-file-matrix.txt')
+for line in fhand:
+	print(line)
+print('Done')
+print('''
+-------------------------------------------------''')
