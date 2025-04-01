1576019759ea4472794bd86c82ee06d0fe805db8
Jim Burnham <jim.the.steam.clown@gmail.com>
2025-03-31 18:07:02 -0700

    Update Ch9-Read-Write-Files-Lab#1--File-Handle.py

--- Ch9-Read-Write-Files-Lab#1--File-Handle.py
+++ Ch9-Read-Write-Files-Lab#1--File-Handle.py
@@ -1,4 +1,7 @@
 # Opening File Handles and reading data from files
+# You can get the following files with wget
+# sudo wget -O text-flie-matrix.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Python-Class-STEAM-Clown/refs/heads/main/text-file-matrix.txt
+# sudo wget -O text-file-mail-very-short.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Python-Class-STEAM-Clown/refs/heads/main/text-file-mail-very-short.txt
 print('''
 This Lab is about opening a file handle, and 
 printing the file handle.
@@ -27,12 +30,12 @@ print('''Answer to Challenge 1
 -------------------------------------------------''')
 # What does the following parts of the file handle mean?
 # 1) _io.TextIOWrapper 
-# 2) name='matrix.txt' 
+# 2) name='text-file-matrix.txt' 
 # 3) mode='r' 
 # 4) encoding='UTF-8'>
 # print('The parts of a file handle are:')
 # -----------------------------
-xfile = open('matrix.txt')
+xfile = open('text-file-matrix.txt')
 print(xfile)
 print('''
 -------------------------------------------------''')
